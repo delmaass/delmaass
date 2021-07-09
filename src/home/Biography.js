00ab96ca960e3134a11ba53432c111bdeea999b5
Louis Delmas <louis.delmas@outlook.com>
2021-07-09 10:27:53 +0200

    Responsive, without animations

--- src/home/Biography.js
+++ src/home/Biography.js
@@ -4,14 +4,14 @@ import wavingHand from "../assets/images/waving-hand.png";
 const Biography = (props) => (
     <div className="bg-gradient-to-b from-white to-blue-300 dark:from-gray-900  dark:to-blue-900">
         <article className="container dark:text-white transition-all">
-            <div className="flex flex-col-reverse md:grid md:grid-cols-3 md:gap-16">
+            <div className="flex flex-col-reverse lg:grid lg:grid-cols-3 lg:gap-16">
 
-                <div className="flex flex-col-reverse md:flex-col">
+                <div className="flex flex-col">
                     <div className="mb-6 h-64 aspect-w-3 aspect-h-4">
                         <img className="object-cover shadow-lg rounded-lg" src={ photoBiography } alt="" />
                     </div>
                     
-                    <div className="flex space-x-6 md:order-2">
+                    <div className="flex space-x-6 lg:order-2">
                         {props.social.map((item) => (
                         <a target="_blank" key={item.name} href={item.href} className="dark:text-gray-200 dark:hover:text-gray-100">
                             <span className="sr-only">{item.name}</span>
@@ -21,10 +21,10 @@ const Biography = (props) => (
                     </div>
                 </div>
 
-                <div className="md:col-span-2 text-lg md:text-xl">
-                    <h3 className="flex flex-row items-center text-2xl md:text-5xl font-bold tracking-tight mb-8">
+                <div className="mb-8 lg:mb-0 lg:col-span-2 text-lg lg:text-xl">
+                    <h3 className="flex flex-row items-center text-3xl lg:text-5xl font-bold tracking-tight mb-8">
                         { props.content.title }
-                        <img className="animate-wave w-12 ml-4" src={ wavingHand } alt="" />
+                        <img className="animate-wave w-10 lg:w-12 ml-4" src={ wavingHand } alt="" />
                     </h3>
                     
                     { props.content.text.split("\/").map((item) => (
--- src/home/End.js
+++ src/home/End.js
@@ -3,21 +3,21 @@ import faceSunglasses from "../assets/images/face-sunglasses.png"
 
 const End = (props) => (
     <footer className="container bg-white dark:bg-gray-900">
-        <div className="flex flex-col md:flex-row justify-between items-center">
-            <h3 className="font-bold text-4xl flex">
+        <div className="flex flex-col lg:flex-row justify-between items-center">
+            <h3 className="font-bold text-xl lg:text-4xl flex items-center">
                 { props.content.title }
-                <img className="w-12 ml-4" src={ faceSunglasses } alt="" />
+                <img className="w-7 h-min lg:w-12 ml-2 lg:ml-4" src={ faceSunglasses } alt="" />
             </h3>
             <a
                 href="mailto:louis.delmas@outlook.com"
-                className="inline-flex items-center px-6 py-3 border border-transparent shadow-sm text-xl font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="inline-flex items-center mt-5 lg:mt-0 px-4 py-2 lg:px-6 lg:py-3 border border-transparent shadow-sm text-lg lg:text-xl font-medium rounded-lg text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
                 { props.content.contact }
                 <MailIcon className="ml-3 -mr-1 h-7 w-7" aria-hidden="true" />
             </a>
         </div>
-        <div className="py-12 md:flex md:items-center md:justify-between">
-            <div className="flex justify-center space-x-6 md:order-2">
+        <div className="py-12 lg:flex lg:items-center lg:justify-between">
+            <div className="flex justify-center space-x-6 lg:order-2">
             {props.social.map((item) => (
                 <a target="_blank" key={item.name} href={item.href} className="text-white hover:text-gray-200">
                 <span className="sr-only">{item.name}</span>
@@ -25,7 +25,7 @@ const End = (props) => (
                 </a>
             ))}
             </div>
-            <div className="mt-8 md:mt-0 md:order-1">
+            <div className="mt-8 lg:mt-0 lg:order-1">
             <p className="text-center text-base text-white">&copy; { props.content.copyright }</p>
             </div>
         </div>
